21c1a434f20c2dd1fbf7bcbb3569463e234f517f
Farhan Ahmed <farhanf7n@gmail.com>
2024-12-11 21:09:51 +0500

    Made Three Cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Engagement from "./components/Engagement";
 import GridPattern from "./components/Grid-pattern";
 import Expertise from "./components/Expertise";
+import ThreeCards from "./components/ThreeCards";
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
       <Engagement />
       <GridPattern />
       <Expertise />
+      <ThreeCards />
     </>
   );
 }
